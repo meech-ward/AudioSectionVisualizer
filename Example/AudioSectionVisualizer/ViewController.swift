a87af84d38874e205733f029eef357ba40e41258
Sam Meech-Ward <sam@meech-ward.com>
2018-01-01 12:59:12 -0700

    Add the audio section view to the example project

--- Example/AudioSectionVisualizer/ViewController.swift
+++ Example/AudioSectionVisualizer/ViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import AudioSectionVisualizer
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var audioSectionView: AudioSectionView!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -19,6 +22,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    var points: [Double] = []
+    
+    for _ in 0..<20 {
+      points.append(Double(arc4random_uniform(100))/100.0)
+    }
+    self.audioSectionView.draw(points: points, usedPointsIndexStart: 10, usedPointsIndexEnd: 15)
+//    self.audioSectionView.draw(fromPercentage: 50, toPercentage: 75, duration: 0.5)
+//    self.audioSectionView.animate()
+  }
 
 }
 
